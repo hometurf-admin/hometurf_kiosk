29e32f921fa22474021f0091c9f9ac91f8870318
hometurf-admin <admin@hometurf.ai>
2025-03-04 20:23:01 +0530

    allow mixes req 3

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,17 @@ const nextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "default-src 'self'; img-src 'self' http: https: data:;",
+            value:
+              "default-src 'self' http: https: data: blob: 'unsafe-inline' 'unsafe-eval'; " +
+              "img-src 'self' http: https: data: blob:; " +
+              "media-src 'self' http: https: data: blob:; " +
+              "connect-src 'self' http: https:; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+              "style-src 'self' 'unsafe-inline';",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
           },
         ],
       },
